3f60bc6fd4cc40eba077f4f6aa9e015c0d56aa0a
rakshithraj <rakshith.r@glowtouch.com>
2024-05-09 14:46:15 +0530

    Moved welcome text to right

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,7 +4,7 @@ import styles from './Nav.module.css'
 import { useNavigate } from 'react-router-dom'
 import { useState } from 'react'
 import {
-  LOG_IN_MENU, SIGN_UP_MENU,LOG_OUT_MENU,HOME_MENU
+  LOG_IN_MENU, SIGN_UP_MENU, LOG_OUT_MENU, HOME_MENU
 } from '../../constants'
 
 const publicMenuItems = [
@@ -12,44 +12,59 @@ const publicMenuItems = [
 ];
 
 const protectedMenuItems = [
-   { menuName: HOME_MENU,path: ''}
+  { menuName: HOME_MENU, path: '' }
 ];
 
 
-const NavBar = ({ user, handleLogout,activeMenu, handleMenuChange}) => {
+const NavBar = ({ user, handleLogout, activeMenu, handleMenuChange }) => {
   //const [active, setActive] = useState(activeMenu)
-  
+
   const _handleClick = (menuItem) => {
     handleMenuChange(menuItem.menuName);
   }
   return (
 
     <nav className={styles.navbar}>
-      {user ?
-        <ul className={styles.navList}>
-          <li>Welcome, {user.firstName+" "+user.lastName}</li>
-          {protectedMenuItems.map((menuItem, index) =>
-            <li key={index}><NavLink to={menuItem.path}
-              className={activeMenu == menuItem.menuName ? styles.navbar_link_active : {}}
-              onClick={_handleClick.bind(this, menuItem)}>{menuItem.menuName}</NavLink></li>
-          )
-          }
-         <li><NavLink to="" onClick={handleLogout}>{LOG_OUT_MENU}</NavLink></li>
-        </ul>
-        :
-        <ul className={styles.navList}>
-          {publicMenuItems.map((menuItem, index) =>
-            <li key={index}><NavLink to={menuItem.path}
-              className={activeMenu == menuItem.menuName ? styles.navbar_link_active : {}}
-              onClick={_handleClick.bind(this, menuItem)}>{menuItem.menuName}</NavLink></li>
-          )}
+      <div >
+        {
+          user ?
+            <ul className={styles.navList}>
+              {protectedMenuItems.map((menuItem, index) =>
+                <li key={index}><NavLink to={menuItem.path}
+                  className={activeMenu == menuItem.menuName ? styles.navbar_link_active : {}}
+                  onClick={_handleClick.bind(this, menuItem)}>{menuItem.menuName}</NavLink></li>
+              )
+              }
+              <li><NavLink to="" onClick={handleLogout}>{LOG_OUT_MENU}</NavLink></li>
+            </ul>
+            :
+            <ul className={styles.navList}>
+              {publicMenuItems.map((menuItem, index) =>
+                <li key={index}><NavLink to={menuItem.path}
+                  className={activeMenu == menuItem.menuName ? styles.navbar_link_active : {}}
+                  onClick={_handleClick.bind(this, menuItem)}>{menuItem.menuName}</NavLink></li>
+              )}
 
-          {/* <li><NavLink to="/auth/login" className={styles.navbar_link_active}>Log In</NavLink></li>
-          <li><NavLink to="/auth/signup">Sign Up</NavLink></li> */}
+              {/* <li><NavLink to="/auth/login" className={styles.navbar_link_active}>Log In</NavLink></li>
+           <li><NavLink to="/auth/signup">Sign Up</NavLink></li> */}
 
 
-        </ul>
-      }
+            </ul>
+        }
+      </div>
+      <div className={styles.logo}>
+
+      </div>
+      <div >
+        {
+          user ?
+            <ul className={styles.navList}>
+              <li>Welcome, {user.firstName + " " + user.lastName}</li>
+            </ul>
+            :
+            <></>
+        }
+      </div>
     </nav>
   )
 }

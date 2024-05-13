1d991cacd1f7496207035cedd18fdf3b7aeb8a39
rakshithraj <rakshith.r@glowtouch.com>
2024-05-13 12:07:17 +0530

    Save resumeId recevied in login response.

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -2,7 +2,7 @@
 import { AppService, HTTPHeaders } from '.'
 import * as tokenService from './tokenService'
 import aes from 'crypto-js/aes';
-import {clearData,setLoggedIn} from '../redux/'
+import {clearData,setLoggedIn,setResumeId} from '../redux/'
 
 import {
   BASE_API_URL,
@@ -30,6 +30,7 @@ export const userService = {
     let jsonResponse = response
     if (jsonResponse.data.User.token){
        tokenService.setSessionToken(jsonResponse.data.User.token)
+       setResumeId(jsonResponse.data.User?.resumeId ?? null);
        setLoggedIn(true)
     }
     return jsonResponse

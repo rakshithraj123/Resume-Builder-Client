ddc8624cbefc36ade0cf8d065b69fa414c8f0832
rakshithraj <rakshith.r@glowtouch.com>
2024-05-20 19:29:58 +0530

    New design Added. landing page.

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,51 @@
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import Logo from '../../assets/img/logo-white.svg';
+import Nav from 'react-bootstrap/Nav';
+
+
+function Footer({ user }) {
+  return (
+    <>
+      <footer className="bg-dark" data-bs-theme="dark">
+        <Container>
+          <Row>
+            <Col>
+              <div class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4">
+                <p class="mb-0 text-body-secondary">© 2024 Resume Builder</p>
+
+                <a href="/" class="mb-3 mb-md-0 link-body-emphasis">
+                  <Image src={Logo} width={40} />
+                </a>
+
+                <Nav >
+                  {
+                    user ? (
+                      <Nav.Item>
+                        <Nav.Link href="/" className="text-body-secondary">Home</Nav.Link>
+                      </Nav.Item>
+                    ) : (
+                      <>
+                        <Nav.Item>
+                          <Nav.Link href="/auth/login" className="text-body-secondary">Login</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                          <Nav.Link href="/auth/signup" className="text-body-secondary">Signup</Nav.Link>
+                        </Nav.Item>
+                      </>
+                    )
+                  }
+                </Nav>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,8 +1,15 @@
 // npm modules
-import { NavLink } from 'react-router-dom'
 import styles from './Nav.module.css'
 import { useNavigate } from 'react-router-dom'
 import { useState } from 'react'
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Image from 'react-bootstrap/Image';
+import Logo from '../../assets/img/logo-white.svg';
+import { Link } from "react-router-dom";
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+
 import {
   LOG_IN_MENU, SIGN_UP_MENU, LOG_OUT_MENU, HOME_MENU
 } from '../../constants'
@@ -23,49 +30,95 @@ const NavBar = ({ user, handleLogout, activeMenu, handleMenuChange }) => {
     handleMenuChange(menuItem.menuName);
   }
   return (
-
-    <nav className={styles.navbar}>
-      <div >
-        {
-          user ?
-            <ul className={styles.navList}>
-              {protectedMenuItems.map((menuItem, index) =>
-                <li key={index}><NavLink to={menuItem.path}
-                  className={activeMenu == menuItem.menuName ? styles.navbar_link_active : {}}
-                  onClick={_handleClick.bind(this, menuItem)}>{menuItem.menuName}</NavLink></li>
+  <Navbar expand="lg" bg="primary" data-bs-theme="dark" fixed="top">
+      <Container>
+        <Navbar.Brand href="/"><Image src={Logo} width={40} /> <span className='text-uppercase fw-bold'>Resume Builder</span></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+          {
+              user?(
+                <>
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/dashboard">Dashboard</Nav.Link>
+                <div className="vr" />        
+                </>           
+              ):(
+                <>              
+                </>
               )
-              }
-              <li><NavLink to="" onClick={handleLogout}>{LOG_OUT_MENU}</NavLink></li>
-            </ul>
-            :
-            <ul className={styles.navList}>
-              {publicMenuItems.map((menuItem, index) =>
-                <li key={index}><NavLink to={menuItem.path}
-                  className={activeMenu == menuItem.menuName ? styles.navbar_link_active : {}}
-                  onClick={_handleClick.bind(this, menuItem)}>{menuItem.menuName}</NavLink></li>
-              )}
+            }
+           
+           
+            {
+              user?(
+                <Button variant="light" onClick={handleLogout}>Log Out</Button>              
+              ):(
+                <>
+                <Nav.Link  href="/auth/login">Log In</Nav.Link>
+                 <Button variant="light" as={Link} to="/auth/signup">Sign Up</Button>
+                </>
+              )
+            }
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
 
-              {/* <li><NavLink to="/auth/login" className={styles.navbar_link_active}>Log In</NavLink></li>
-           <li><NavLink to="/auth/signup">Sign Up</NavLink></li> */}
 
 
-            </ul>
-        }
-      </div>
-      <div className={styles.logo}>
 
-      </div>
-      <div >
-        {
-          user ?
-            <ul className={styles.navList}>
-              <li>Welcome, {user.firstName + " " + user.lastName}</li>
-            </ul>
-            :
-            <></>
-        }
-      </div>
-    </nav>
+
+
+
+
+
+
+
+
+
+    // <nav className={styles.navbar}>
+    //   <div >
+    //     {
+    //       user ?
+    //         <ul className={styles.navList}>
+    //           {protectedMenuItems.map((menuItem, index) =>
+    //             <li key={index}><NavLink to={menuItem.path}
+    //               className={activeMenu == menuItem.menuName ? styles.navbar_link_active : {}}
+    //               onClick={_handleClick.bind(this, menuItem)}>{menuItem.menuName}</NavLink></li>
+    //           )
+    //           }
+    //           <li><NavLink to="" onClick={handleLogout}>{LOG_OUT_MENU}</NavLink></li>
+    //         </ul>
+    //         :
+    //         <ul className={styles.navList}>
+    //           {publicMenuItems.map((menuItem, index) =>
+    //             <li key={index}><NavLink to={menuItem.path}
+    //               className={activeMenu == menuItem.menuName ? styles.navbar_link_active : {}}
+    //               onClick={_handleClick.bind(this, menuItem)}>{menuItem.menuName}</NavLink></li>
+    //           )}
+
+    //           {/* <li><NavLink to="/auth/login" className={styles.navbar_link_active}>Log In</NavLink></li>
+    //        <li><NavLink to="/auth/signup">Sign Up</NavLink></li> */}
+
+
+    //         </ul>
+    //     }
+    //   </div>
+    //   <div className={styles.logo}>
+
+    //   </div>
+    //   <div >
+    //     {
+    //       user ?
+    //         <ul className={styles.navList}>
+    //           <li>Welcome, {user.firstName + " " + user.lastName}</li>
+    //         </ul>
+    //         :
+    //         <></>
+    //     }
+    //   </div>
+    // </nav>
   )
 }
 

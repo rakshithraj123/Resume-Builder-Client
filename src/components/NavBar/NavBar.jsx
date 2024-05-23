60c66cd187e330594727e5df798f8d0cb3d832c4
rakshithraj <rakshith.r@glowtouch.com>
2024-05-23 12:16:44 +0530

    Navigation header menu chnages.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,9 +9,10 @@ import Logo from '../../assets/img/logo-white.svg';
 import { Link } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 import {
-  LOG_IN_MENU, SIGN_UP_MENU, LOG_OUT_MENU, HOME_MENU
+  LOG_IN_MENU, SIGN_UP_MENU, LOG_OUT_MENU, HOME_MENU, USER_NAME_MENU
 } from '../../constants'
 
 const publicMenuItems = [
@@ -19,9 +20,11 @@ const publicMenuItems = [
 ];
 
 const protectedMenuItems = [
-  { menuName: HOME_MENU, path: '' }
+  { menuName: HOME_MENU, path: '/' }, { menuName: USER_NAME_MENU, path: '' }
 ];
 
+const dropDownMenuItems = [
+];
 
 const NavBar = ({ user, handleLogout, activeMenu, handleMenuChange }) => {
   //const [active, setActive] = useState(activeMenu)
@@ -30,16 +33,71 @@ const NavBar = ({ user, handleLogout, activeMenu, handleMenuChange }) => {
     handleMenuChange(menuItem.menuName);
   }
   return (
-  <Navbar expand="lg" bg="primary" data-bs-theme="dark" fixed="top">
+    <Navbar expand="lg" bg="primary" data-bs-theme="dark" fixed="top">
       <Container>
         <Navbar.Brand href="/"><Image src={Logo} width={40} /> <span className='text-uppercase fw-bold'>Resume Builder</span></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-          {
+            {
+              user ? (
+
+                <>
+                  {protectedMenuItems.map((menuItem, index) =>
+                    (menuItem.menuName == USER_NAME_MENU) ? (
+                      <Dropdown>
+                        <Dropdown.Toggle id="dropdown-basic">
+                          <i class="bi bi-person"></i> <span>{user.firstName + " " + user.lastName}</span>
+                        </Dropdown.Toggle>
+
+                        <Dropdown.Menu>
+                            <>
+                            {
+                            dropDownMenuItems.map((menuItem, index) =>
+                              (<Dropdown.Item key={index}
+                                href={menuItem.path}
+                               onClick={_handleClick.bind(this, menuItem)}
+                              ><i class="bi bi-box-arrow-right me-3"></i> {menuItem.menuName}</Dropdown.Item>)
+                            )
+                          }
+                            <Dropdown.Item 
+                           onClick={handleLogout}
+                          ><i class="bi bi-box-arrow-right me-3"></i> {LOG_OUT_MENU}</Dropdown.Item>
+                           </>
+                         
+                        </Dropdown.Menu>
+                      </Dropdown>
+                    ) : (
+                      <Nav.Link
+                        className={activeMenu === menuItem.menuName ? "active" : ""}
+                        key={index}
+                        href={menuItem.path}
+                        onClick={_handleClick.bind(this, menuItem)}>{menuItem.menuName}</Nav.Link>
+                    )
+                  )
+                  }
+                </>
+              ) : (
+                <>
+                  {publicMenuItems.map((menuItem, index) =>
+                    (menuItem.menuName == SIGN_UP_MENU) ? (
+                      <Button variant="light" as={Link} to={menuItem.path}
+                        onClick={_handleClick.bind(this, menuItem)}>{menuItem.menuName}</Button>
+                    ) : (
+                      <Nav.Link
+                        key={index}
+                        href={menuItem.path}
+                        onClick={_handleClick.bind(this, menuItem)}>{menuItem.menuName}</Nav.Link>
+                    )
+                  )
+                  }
+                </>
+              )
+            }
+            {/* {
               user?(
                 <>
-                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/" >Home</Nav.Link>
                 <Nav.Link href="/dashboard">Dashboard</Nav.Link>
                 <div className="vr" />        
                 </>           
@@ -59,7 +117,7 @@ const NavBar = ({ user, handleLogout, activeMenu, handleMenuChange }) => {
                  <Button variant="light" as={Link} to="/auth/signup">Sign Up</Button>
                 </>
               )
-            }
+            } */}
           </Nav>
         </Navbar.Collapse>
       </Container>

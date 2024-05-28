00760c5ff79da18ad94a3ee66a6e73961650552a
ananthakrishna.k <ananthakrishna.k@glowtouch.com>
2024-05-28 19:41:26 +0530

    UI font increased in preview resume

--- src/pages/PreviewResume/PreviewResume.js
+++ src/pages/PreviewResume/PreviewResume.js
@@ -11,9 +11,7 @@ import styles from "./PreviewResume.module.css";
 import { useLocation } from "react-router-dom";
 import { resumeAddService } from "../../services/resumeAdd.service";
 import { toast } from "react-toastify";
-import {
-  CREATE_RESUME_MENU
-} from '../../constants'
+import { CREATE_RESUME_MENU } from "../../constants";
 
 const PreviewResume = ({ handleNavigation, savedResumeId }) => {
   const { state } = useLocation();
@@ -21,16 +19,16 @@ const PreviewResume = ({ handleNavigation, savedResumeId }) => {
 
   let resumeId = state?.resumeId;
   if (resumeId == null) {
-    resumeId = savedResumeId
+    resumeId = savedResumeId;
   }
   const [resumeData, setResumeData] = useState({});
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        setLoading(true)
+        setLoading(true);
         const response = await resumeAddService.fetch(resumeId);
-        
+
         console.log(response);
 
         if (response.status) {
@@ -41,16 +39,15 @@ const PreviewResume = ({ handleNavigation, savedResumeId }) => {
       } catch (error) {
         console.error("Error:", error);
         // Handle error
-      } finally{
-        setLoading(false)
+      } finally {
+        setLoading(false);
       }
     };
     if (resumeId == null) {
-      handleNavigation()
+      handleNavigation();
     } else {
       fetchData();
     }
-
   }, []);
 
   const printResume = (e) => {
@@ -65,12 +62,23 @@ const PreviewResume = ({ handleNavigation, savedResumeId }) => {
 
   const goToResumeEdit = (e) => {
     e.preventDefault();
-    handleNavigation(CREATE_RESUME_MENU, { resumeData: resumeData, resumeId: resumeId })
+    handleNavigation(CREATE_RESUME_MENU, {
+      resumeData: resumeData,
+      resumeId: resumeId,
+    });
   };
 
   if (loading)
     return (
-      <div class="text-center" style={{ height: "calc(100vh - 100px)", justifyContent: 'center', alignItems: "center", display: "flex" }}>
+      <div
+        class="text-center"
+        style={{
+          height: "calc(100vh - 100px)",
+          justifyContent: "center",
+          alignItems: "center",
+          display: "flex",
+        }}
+      >
         <div class="spinner-border" role="status">
           <span class="sr-only"></span>
         </div>
@@ -79,18 +87,25 @@ const PreviewResume = ({ handleNavigation, savedResumeId }) => {
 
   return (
     <>
-
-
       <div className="p-5 bg-primary">
         <Container>
           <Row className="align-items-center">
             <Col>
               <div className="py-5">
-                <h3 className="text-white"><i class="bi bi-journal-check"></i> Preview your resume</h3>
+                <h3 className="text-white">
+                  <i class="bi bi-journal-check"></i> Preview your resume
+                </h3>
               </div>
             </Col>
             <Col md="auto">
-              <Button variant="outline-light" onClick={(e)=>{printResume(e)}}>Print / Download</Button>
+              <Button
+                variant="outline-light"
+                onClick={(e) => {
+                  printResume(e);
+                }}
+              >
+                Print / Download
+              </Button>
             </Col>
           </Row>
         </Container>
@@ -99,40 +114,50 @@ const PreviewResume = ({ handleNavigation, savedResumeId }) => {
       <Container className="bg-white rounded-top p-md-5 p-3 mt-n5 shadow">
         <Row>
           <Col>
-
-
-
             <>
-            <div className="bg-white shadow-lg p-3" id="content-to-print">
-              <Row className=" justify-content-between mb-5">
-                <Col>
-                  <h3>{resumeData.firstName} {resumeData.lastName}</h3></Col>
-                <Col className="text-end">
-                  <h5>{resumeData.phoneNumber}</h5>
-                  <h5>{resumeData.email}</h5>
-                </Col>
-              </Row>
+              <div className="bg-white shadow-lg p-3" id="content-to-print">
+                <Row className=" justify-content-between mb-5">
+                  <Col>
+                    <h3 className={`${styles.largeFont}`}>
+                      {resumeData.firstName} {resumeData.lastName}
+                    </h3>
+                  </Col>
+                  <Col className="text-end">
+                    <h5 className={`${styles.defaultFont}`}>
+                      {resumeData.phoneNumber}
+                    </h5>
+                    <h5 className={`${styles.mediumFont}`}>
+                      {resumeData.email}
+                    </h5>
+                  </Col>
+                </Row>
 
-              <p className="mb-5">{resumeData.objective}</p>
-              <h4 className="mb-3">Key Skills</h4>
-              <ul className="list-unstyled ps-4 mb-5">
-              {(resumeData["keySkills"] ?? []).map((item, index) => (
+                <p className={`mb-5 ${styles.defaultFont}`}>
+                  {resumeData.objective}
+                </p>
+                <h4 className={`mb-3 ${styles.largeFont}`}>Key Skills</h4>
+                <ul className={`list-unstyled ps-4 mb-5 ${styles.defaultFont}`}>
+                  {(resumeData["keySkills"] ?? []).map((item, index) => (
                     <li key={index}>➢{item}</li>
                   ))}
-              </ul>
+                </ul>
 
-              <h4 className="mb-3">Professional Experience</h4>
-              {(resumeData["professionalExperience"] ?? []).map(
+                <h4 className={`mb-3 ${styles.largeFont}`}>
+                  Professional Experience
+                </h4>
+                {(resumeData["professionalExperience"] ?? []).map(
                   (experience, experienceIndex) => (
                     <div key={experienceIndex}>
-                      <h5 className="mb-4">{experience.company_name}</h5>
-                   
+                      <h5 className={`mb-4 ${styles.mediumFont}`}>
+                        {experience.company_name}
+                      </h5>
+
                       {experience.work.map((item, workIndex) => (
                         <div key={workIndex}>
-                                         
-                          <h6 className="mb-4">{item.designation}</h6>
-                             <ul className="lps-4 mb-5">
-                         
+                          <h6 className={`mb-4 ${styles.smallFont}`}>
+                            {item.designation}
+                          </h6>
+                          <ul className={`lps-4 mb-5 ${styles.defaultFont}`}>
                             {item.work_details.map(
                               (workDetail, workDetailIndex) => (
                                 <li key={workDetailIndex}>{workDetail}</li>
@@ -144,42 +169,38 @@ const PreviewResume = ({ handleNavigation, savedResumeId }) => {
                     </div>
                   )
                 )}
-                <h4 className="mb-3">Education</h4>
+                <h4 className={`mb-3 ${styles.largeFont}`}>Education</h4>
 
-                <ul className="lps-4 mb-5">
-                {(resumeData["education"] ?? []).map((item, index) => (
+                <ul className={`lps-4 mb-5 ${styles.defaultFont}`}>
+                  {(resumeData["education"] ?? []).map((item, index) => (
                     <li key={index}>{item}</li>
                   ))}
-</ul>
+                </ul>
 
+                <h4 className={`mb-3 ${styles.largeFont}`}>
+                  Additional Qualifications
+                </h4>
 
-<h4 className="mb-3">Additional Qualifications</h4>
-
-<ul className="lps-4 mb-5">
-{(resumeData["additionalQualifications"] ?? []).map((item, index) => (
-                    <li key={index}>{item}</li>
-                  ))}
-</ul>
-
-
-            </div>
-
-
+                <ul className={`lps-4 mb-5 ${styles.defaultFont}`}>
+                  {(resumeData["additionalQualifications"] ?? []).map(
+                    (item, index) => (
+                      <li key={index}>{item}</li>
+                    )
+                  )}
+                </ul>
+              </div>
             </>
-           
-
-
-
 
             <div className="text-center pt-4">
-              <Button variant="primary" onClick={(e) => goToResumeEdit(e)}>EDIT</Button>
+              <Button variant="primary" onClick={(e) => goToResumeEdit(e)}>
+                EDIT
+              </Button>
             </div>
           </Col>
         </Row>
       </Container>
-
     </>
-  )
+  );
 
   // return (
   //   <div>
